6d39d14b2018bd8be38cb6b6120450f5125c39d7
wnjenga <wnjenga@safaricom.co.ke>
2023-07-08 21:39:20 +0300

    updated files

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb+srv://wnjenga:rlkQEJrPmN7aCPNx@cluster0.exzgxhs.mongodb.net/?retryWrites=true&w=majority/';
+let mongodb_url = 'mongodb+srv://wnjenga:8Hieb7insLhcoeN6@cluster0.exzgxhs.mongodb.net/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
